99e15192b44ffcabb1e17943637e7a21dd2cde11
Nolthawat Deangharn <nolthawat.d@gmail.com>
2025-05-14 10:16:53 +0700

    feat: init socket

---
+++ src/app/page.tsx
@@ -0,0 +1,52 @@
+"use client";
+
+import Image from "next/image";
+import styles from "./page.module.css";
+
+import { useEffect, useState } from "react";
+import { socket } from "../socket";
+
+export default function Home() {
+	const [isConnected, setIsConnected] = useState(false);
+	const [transport, setTransport] = useState("N/A");
+
+	useEffect(() => {
+		if (socket.connected) {
+			onConnect();
+		}
+
+		function onConnect() {
+			setIsConnected(true);
+			setTransport(socket.io.engine.transport.name);
+
+			socket.io.engine.on("upgrade", (transport: any) => {
+				setTransport(transport.name);
+			});
+		}
+
+		function onDisconnect() {
+			setIsConnected(false);
+			setTransport("N/A");
+		}
+
+		socket.on("connect", onConnect);
+		socket.on("disconnect", onDisconnect);
+
+		return () => {
+			socket.off("connect", onConnect);
+			socket.off("disconnect", onDisconnect);
+		};
+	}, []);
+
+	return (
+		<div className={styles.page}>
+			<main className={styles.main}>
+				<div>
+					<p>Status: {isConnected ? "connected" : "disconnected"}</p>
+					<p>Transport: {transport}</p>
+				</div>
+			</main>
+			<footer className={styles.footer}></footer>
+		</div>
+	);
+}
